4c3aa6290af1039da1696755f6410eede036ac0e
alicekaerast <alice_kaerast@flutterstudios.com>
2024-12-20 16:13:07 +0100

    add metrics and cleaner logs

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"fmt"
+  "log/slog"
 	"net"
 	"net/http"
 	"os"
 	"time"
+  "github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var logger *slog.Logger
 
 func getColor() string {
 	return os.Getenv("COLOR")
@@ -38,21 +42,21 @@ func serveTCP() {
 func httpHandler(w http.ResponseWriter, r *http.Request) {
 	color := getColor()
 
-	fmt.Printf("Serving color: #%s", color)
-	fmt.Println()
+  logger.Info("Serving color", "color", color)
 
 	fmt.Fprintf(w, "<body bgcolor=\"#%s\"><h1>#%s</h1></body>", color, color)
 }
 
 func main() {
+  logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	color := getColor()
 
-	fmt.Printf("Booted with color: #%s", color)
-	fmt.Println()
+  logger.Info("Booted", "color", color)
 
 	go serveTCP()
 
 	http.HandleFunc("/", httpHandler)
-	fmt.Println("listening with http on :8080 and tcp on :8081")
+  http.Handle("/metrics", promhttp.Handler())
+  logger.Info("listening with http on :8080 and tcp on :8081")
 	http.ListenAndServe(":8080", nil)
 }
